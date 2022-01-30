f08b856433293c30574180cc125174c59a2a8f1e
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-30 11:30:35 +0200

    add profile view and template. configure profile page url

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from app import views
 
 urlpatterns = [
     path('', views.index, name='home page'),
-    path('', views.index, name='home page'),
+    path('about/', TemplateView.as_view(template_name='profile.html'), name='profile')
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from django.views.generic import TemplateView
+
 
 def index(request):
     return render(request, 'index.html')
 
 
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
