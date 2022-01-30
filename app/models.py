5c4c718a52ea3f864df0ccffdf302dbbe0b5c534
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-30 11:07:26 +0200

    create profile model and register it in admin section

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    about = models.CharField(max_length=200)
+    image_url = models.URLField()
