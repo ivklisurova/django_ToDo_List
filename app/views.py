0806f3f8f9eb850292fd69008d9050c3a312aa45
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-31 15:44:05 +0200

    update profile class with content to render

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
+from app.models import Profile
+
 
 def index(request):
     return render(request, 'index.html')
@@ -10,3 +12,11 @@ def index(request):
 
 class ProfileView(TemplateView):
     template_name = 'profile.html'
+    profile_model = Profile
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['my_profile'] = self.profile_model.objects.get()
+        return data
+
+
