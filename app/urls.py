a2d2248fb5ec6913c9e253301e2c570720aa0060
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-02-02 21:50:54 +0200

    add login view and template

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from app import views
-from app.views import RegisterView
+from app.views import RegisterView, LoginUserView
 
 urlpatterns = [
     path('', views.index, name='home page'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
     # path('profile/', TemplateView.as_view(template_name='userprofile.html'), name='profile')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
 # Create your views here.
@@ -17,6 +18,11 @@ class RegisterView(CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy('home page')
     template_name = 'profile/register.html'
+
+
+class LoginUserView(LoginView):
+    template_name = 'profile/login.html'
+    # redirect_authenticated_user = True
 
 
 class ProfileView(TemplateView):
