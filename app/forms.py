47d85bf1906c521a3ef603afc331fe6609b8ae73
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-02-02 15:01:34 +0200

    fix form , add related to registration fields and add crispy form layout to template

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -6,9 +8,18 @@ from app.models import Profile
 
 
 class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(label='Enter password',
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password',
+        widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email',)
+        help_texts = {
+            'username': None,
+            'email': None,
+        }
 
     def clean_email(self):
         email = self.cleaned_data.get('email', False)
