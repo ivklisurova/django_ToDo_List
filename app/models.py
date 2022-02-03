49700036d6e929d21d75766c23c9bf4722405284
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-02-03 22:34:25 +0200

    update profile model

--- app/models.py
+++ app/models.py
@@ -5,8 +5,6 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    about = models.CharField(max_length=200)
-    image_url = models.URLField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.png', upload_to='profile_images')
+    bio = models.TextField()
