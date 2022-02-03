ea60c10dbbcfa3c44a9fea238263ff80ae4601c2
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-02-03 22:41:55 +0200

    add media root to settings.py and update urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app import views
 from app.views import RegisterView, LoginUserView
@@ -11,4 +13,4 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', TemplateView.as_view(template_name='profile/account.html'), name='accounts')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
