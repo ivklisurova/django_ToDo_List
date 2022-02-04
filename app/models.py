61b796bca319a851980428bb999aa4af700a0a03
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-02-04 22:30:57 +0200

    add receiver - create or update user profile

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default='default.png', upload_to='profile_images')
     bio = models.TextField()
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
